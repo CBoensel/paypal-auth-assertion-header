44bb182ef9c18c4860ae6658cc9f2c945a6ad4e4
Carsten Bönsel <CBoensel@users.noreply.github.com>
2022-03-30 20:51:31 +0200

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+  import Header from "./components/Header.svelte";
+  import Footer from "./components/Footer.svelte";
+  import CreateHeaderForm from "./components/CreateHeaderForm.svelte";
+
+  export let title;
+</script>
+
+<Header />
+<main>
+  <h1>{title}</h1>
+  <p>Lorem ipsum...</p>
+
+  <CreateHeaderForm />
+</main>
+<Footer />
+
+<style>
+  main {
+    width: 100%;
+    max-width: 960px;
+    margin: 40px auto;
+  }
+</style>
---
+++ src/components/Header.svelte
@@ -0,0 +1,19 @@
+<header>
+  <h1>
+    <img src="/img/paypal-logo.svg" alt="PayPal Logo" />
+  </h1>
+</header>
+
+<style>
+  header {
+    background: #f7f7f7;
+    padding: 20px;
+  }
+  h1 {
+    margin: 0;
+    text-align: center;
+  }
+  img {
+    max-width: 180px;
+  }
+</style>
