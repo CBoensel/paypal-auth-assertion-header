1d49eb76e5e5afca3b94370cf88f70ca0ee4fb05
Carsten Bönsel <CBoensel@users.noreply.github.com>
2022-04-04 10:06:27 +0200

    Move main content to HeaderInfo module

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,14 @@
 <script>
-  import Header from "./components/Header.svelte";
-  import Footer from "./components/Footer.svelte";
-  import CreateHeaderForm from "./components/CreateHeaderForm.svelte";
+  import Header from './components/Header.svelte';
+  import Footer from './components/Footer.svelte';
+  import CreateHeaderForm from './components/CreateHeaderForm.svelte';
+  import HeaderInfo from './components/HeaderInfo.svelte';
 
   export let title;
 </script>
 
 <svelte:head>
-	<title>{title}</title>
+  <title>{title}</title>
 </svelte:head>
 
 <Header />
@@ -15,20 +16,7 @@
   <div>
     <h1>{title}</h1>
 
-    <p>
-      As described in the <a href="https://developer.paypal.com/docs/multiparty/issue-refund/#link-generatepaypalauthassertionheader#eyJhbGciOiJub25lIn0.eyJpc3MiOiJjbGllbnRfaWQiLCJlbWFpbCI6Im15LWVtYWlsQGV4YW1wbGUuY29tIn0.">Multiparty Paments Developer Docs</a>
-    </p>
-
-    <p>The basic formatting and encoding of the PayPal-Auth-Assertion-Header is:</p>
-
-    <pre>
-      <code>
-        const part1 = base64Encode('{'{'}"alg":"none"{'}'}');
-        const part2 = base64Encode(`{'{'}"iss":${'{'}clientID{'}'},"payer_id":${'{'}merchantID{'}'}{'}'}`);
-
-        console.log(`${'{'}part1{'}'}.${'{'}part2{'}'}.`)
-      </code>
-    </pre>
+    <HeaderInfo />
   </div>
 
   <CreateHeaderForm />
@@ -36,22 +24,17 @@
 <Footer />
 
 <style>
-
   main {
     width: 100%;
     max-width: 960px;
     margin: 40px auto;
   }
+
   h1 {
     margin-bottom: 40px;
   }
 
   div {
     text-align: center;
-  }
-
-  pre {
-    text-align: left;
-    margin-left: 160px;
   }
 </style>
---
+++ src/components/HeaderInfo.svelte
@@ -0,0 +1,24 @@
+<p>
+  As described in the <a
+    href="https://developer.paypal.com/docs/multiparty/issue-refund/#link-generatepaypalauthassertionheader#eyJhbGciOiJub25lIn0.eyJpc3MiOiJjbGllbnRfaWQiLCJlbWFpbCI6Im15LWVtYWlsQGV4YW1wbGUuY29tIn0."
+    >Multiparty Payments Developer Docs</a
+  >
+</p>
+
+<p>The basic formatting and encoding of the PayPal-Auth-Assertion-Header is:</p>
+
+<pre>
+  <code>
+    const part1 = base64Encode('{'{'}"alg":"none"{'}'}');
+    const part2 = base64Encode(`{'{'}"iss":${'{'}clientID{'}'},"payer_id":${'{'}merchantID{'}'}{'}'}`);
+
+    console.log(`${'{'}part1{'}'}.${'{'}part2{'}'}.`)
+  </code>
+</pre>
+
+<style>
+  pre {
+    text-align: left;
+    margin-left: 160px;
+  }
+</style>
