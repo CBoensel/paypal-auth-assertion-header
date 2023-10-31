b326498758bcaeeee49431319d095f6e53b979f9
Carsten Boensel <cboensel@paypal.com>
2023-10-31 13:43:43 +0100

    Decrypt inverted keys as well

--- src/shared/utils.js
+++ src/shared/utils.js
@@ -19,9 +19,14 @@ function decodeThirdPartyAuth(authAssertionHeader) {
   if (authAssertionHeader) {
     const auth2 = authAssertionHeader.split('.')[1];
     const decodedAuth2 = decode(auth2);
-    const [iss, payer_id] = decodedAuth2.split(',');
-    clientID = iss.replace('{"iss":', '').replaceAll('"', '');
-    merchantID = payer_id.replace('"payer_id":', '').replace('}', '').replaceAll('"', '');
+    // const [iss, payer_id] = decodedAuth2.split(',');
+    // clientID = iss.replace('{"iss":', '').replaceAll('"', '');
+    // merchantID = payer_id.replace('"payer_id":', '').replace('}', '').replaceAll('"', '');
+
+    // support object where keys are inverted: example > "eyJhbGciOiJub25lIn0=.eyJwYXllcl9pZCI6IlVUSlRVS0JXQTlVUFkiLCJpc3MiOiJBWEJCWHN6SjFfRTFERGlvMHhsM3dkTFNqblA4RHVQQmdKNllWbzJKa09lVGZhSE4wYTdJQ0xUeEExaDR6eG43WkI3R216UFBfY3BGYjFUaCJ9."
+    const parsedAuth2 = JSON.parse(decodedAuth2); // todo try catch
+    clientID = parsedAuth2.iss;
+    merchantID = parsedAuth2.payer_id;
   }
 
   return [clientID, merchantID];
